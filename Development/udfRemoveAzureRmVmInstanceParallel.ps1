5ccfd9e94fb3b5ec0f7b6dd90050a26099a73040
Chris Langford <clangford@lang-ford.uk>
2019-10-16 13:32:50 +0100

    Updated files names and added helper info

--- Development/udfRemoveAzureRmVmInstanceParallel.ps1
+++ Development/udfRemoveAzureRmVmInstanceParallel.ps1
@@ -1,4 +1,27 @@
-function Remove-AzureRmVmInstanceparallel {
+<#
+Copyright (c) Chris Langford. All rights reserved.
+Licensed under the MIT License.
+#>
+
+<#
+.SYNOPSIS
+Remove Virtual Machine and resources in parallel.
+
+.DESCRIPTION
+This Azure Automation runbook removes all the virtual machines and their resources from
+the chosen Resource group in parallel.
+
+
+Prerequisite: an Azure Automation account with an Azure Run As account credential.
+
+.PARAMETER resourceGroupName
+The Azure resource group name.
+
+.PARAMETER vmName
+The virtual machine name, Regex can be used.
+#>
+
+function Remove-udfAzureRmVmInstanceparallel {
     [cmdletbinding()]
     param (
         # Name of Resource Group        
@@ -48,12 +71,8 @@ function Remove-AzureRmVmInstanceparallel {
                 $managedDisk = $vm.StorageProfile.OsDisk.ManagedDisk
                 ($osDisk + $dataDisk)
                 $nics | ForEach-Object ID
-                $nsg | ForEach-Object ID
-                $nsgName = (Get-AzureRmNetworkSecurityGroup -ResourceGroupName $resourceGroup).Name
-                $vNet | ForEach-Object ID
-                $vNetname = (Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup).Name
 
-                Write-Warning -Message "Deleting Virtual Machine: $($vmName.Name) from Resource Group: $resourceGroup"
+                Write-Output "Deleting Virtual Machine: $($vmName.Name) from Resource Group: $resourceGroup"
 
                 # Delete the Virtual Machine
                 $VM | Remove-AzureRmVM -Force -Confirm:$false
@@ -62,7 +81,7 @@ function Remove-AzureRmVmInstanceparallel {
                 $nics | Where-Object {$_.ID} | ForEach-Object 
                 {
                     $nicName = Split-Path $_.ID -Leaf
-                    Write-Warning -Message "Removing NIC: $nicName"
+                    Write-Output "Removing NIC: $nicName"
                     $nic = Get-AzureRmNetworkInterface -ResourceGroupName $resourceGroup -Name $nicName
                     $nic | Remove-AzureRmNetworkInterface -Force
 
@@ -72,24 +91,19 @@ function Remove-AzureRmVmInstanceparallel {
                         $nic.IpConfigurations.PublicIpAddress | Where-Object {$_.ID} | ForEach-Object
                         {
                             $publicIpName = Split-Path $_.ID -Leaf
-                            Write-Warning -Message "Removing the Public IP: $publicIpName"
+                            Write-Output "Removing the Public IP: $publicIpName"
                             $publicIp = Get-AzureRmPublicIpAddress -ResourceGroupName $resourceGroup -Name $publicIpName
                             $publicIp | Remove-AzureRmPublicIpAddress -Force
                         }
                     }#$removePublicIP
                 }#$nics
 
-                # Delete vNet. This will delete all Virtual networks in the resource group
-                Write-Warning -Message "Removing Virtual Network: $vNetName"
-                $vNet = Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -Name $vNetname
-                $vNet | Remove-AzureRmVirtualNetwork -Force
-
                 # Delete managed disks
                 if($managedDisk)
                 {
                     ($osDisk + $dataDisk) | Where-Object {$_.ID} | ForEach-Object
                     {
-                        Write-Warning -Message "Removing Disk: $_"
+                        Write-Output "Removing Disk: $_"
                         Get-AzureRmDisk -ResourceGroupName $resourceGroup -DiskName $_ | Remove-AzureRmDisk -Force
                     }
                 } else {
@@ -108,17 +122,12 @@ function Remove-AzureRmVmInstanceparallel {
                     $disk = $VM.StorageProfile.OsDisk.Vhd.Uri | Split-Path -Leaf
                     $sa = Get-AzureRmStorageAccount -ResourceGroupName $resourceGroup -Name $saName
                     Get-AzureStorageContainer -Name vhds -Context $sa.Context | Get-AzureStorageBlob -Blob $disk | Remove-AzureStorageBlob
-                }
-
-                # Delete Network Security Group (NSG). This will remove all NSG's in the resource group
-                Write-Warning -Message "Removing Network Security Group: $nsgName"
-                $nsg = Get-AzureRmNetworkSecurityGroup -ResourceGroupName $resourceGroup -Name $nsgName
-                $nsg | Remove-AzureRmNetworkSecurityGroup -Force
+                }                
 
             }
             catch {
-                Write-Warning -Message "You must save your Context first"
-                Write-Warning -Message $_
+                Write-Output "You must save your Context first"
+                Write-Output $_
             }
         }#Start-Job
 
@@ -132,7 +141,19 @@ function Remove-AzureRmVmInstanceparallel {
         Start-Sleep -Seconds 30
         $jobs | Wait-Job | Receive-Job
     } else {
-        Write-Warning -Message "Run the following to view the status of parallel delete'nGet-Job | Receive-Job -Keep"
+        Write-Output "Run the following to view the status of parallel delete'nGet-Job | Receive-Job -Keep"
     }
+
+    # Delete vNet. This will delete all Virtual networks in the resource group
+    $vNetname = (Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup).Name
+    Write-Output "Removing Virtual Network: $vNetName"
+    $vNet = Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -Name $vNetname
+    $vNet | Remove-AzureRmVirtualNetwork -Force
+
+    # Delete Network Security Group (NSG). This will remove all NSG's in the resource group
+    $nsgName = (Get-AzureRmNetworkSecurityGroup -ResourceGroupName $resourceGroup).Name
+    Write-Output "Removing Network Security Group: $nsgName"
+    $nsg = Get-AzureRmNetworkSecurityGroup -ResourceGroupName $resourceGroup -Name $nsgName
+    $nsg | Remove-AzureRmNetworkSecurityGroup -Force
     
 }#function
