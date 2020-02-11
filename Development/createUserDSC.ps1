4632754a21fc6075dcbd1a1446ac64839752f6e7
Chris Langford <clangford@lang-ford.uk>
2020-02-11 08:46:31 +0000

    Added DSC user config script

---
+++ Development/createUserDSC.ps1
@@ -0,0 +1,16 @@
+<#
+User [string] #ResourceName
+{
+    UserName = [string]
+    [ Description = [string] ]
+    [ Disabled = [bool] ]
+    [ FullName = [string] ]
+    [ Password = [PSCredential] ]
+    [ PasswordChangeNotAllowed = [bool] ]
+    [ PasswordChangeRequired = [bool] ]
+    [ PasswordNeverExpires = [bool] ]
+    [ DependsOn = [string[]] ]
+    [ Ensure = [string] { Absent | Present }  ]
+    [ PsDscRunAsCredential = [PSCredential] ]
+}
+#>
