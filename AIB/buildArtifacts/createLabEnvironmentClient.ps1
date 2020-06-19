c8263192ae60470b552f7e4ff98bdad0e79fc662
cnomadl <clangford@lang-ford.uk>
2020-06-19 17:39:35 +0100

    Added messages to tested files

--- AIB/buildArtifacts/createLabEnvironmentClient.ps1
+++ AIB/buildArtifacts/createLabEnvironmentClient.ps1
@@ -1,18 +1,48 @@
-#Download and install the required testing environments features.
+#Download and install the required testing environments features for the Windows 10 Client.
 
 # Install the testing software
-## Create download folder
-New-Item C:\PSDownloads -ItemType Directory
+## Create a temporary download folder
+Write-Warning -MessageData 'Creating a temporary working directory for PowerShell downloads' -InformationAction Continue
+New-Item D:\PSDownloads -ItemType Directory
 
-### Download and install compass
-Invoke-WebRequest -Uri https://downloads.certiport.com/Admin/CertiportConsole/Compass_Setup.exe -OutFile C:\PSDownloads\Compass_Setup.exe -Wait
+## Download and install Compass for MTA exams
+Invoke-WebRequest -Uri http://downloads.certiport.com/Admin/CertiportConsole/Compass_Setup.exe -OutFile D:\PSDownloads\Compass_Setup.exe
 
+Write-Information -MessageData 'Installing Certiport Compass' -InformationAction Continue
 $compassArgs = @('/Silent','path="C:\Certiport\Compass"','/TestCenterID 90040934','/CertiportID 90040934','/TestCenterName "Baltic Training Services"','/Iuser ITAdmin','/Ipwd Dfe500tx08','/UpdateSchedule Daily','/UpdateTime 09:00:00','/LanguageCode ENU')
-Start-Process -FilePath "C:\PSDownloads/Compass_Setup.exe" -ArgumentList $compassArgs -Wait
+Start-Process -FilePath "D:\PSDownloads/Compass_Setup.exe" -ArgumentList $compassArgs -Wait
 
+### Install exams
+Write-Information -MessageData 'Connecting to your Azure storage account' -InformationAction Continue
+Connect-AzAccount
 
-### Download and install SecureClient
-Invoke-WebRequest -Uri https://evolve.cityandguilds.com/secureassess/SecureClientinstaller.msi -OutFile C:\PSDownloads\SecureClientinstaller.msi -Wait
+### Connect to Azure storage
+$StorageAccountName = "balticaibsa"
+$saResourceGroup = "BalticImagesRg"
+$storageAccountKey = Get-AzStorageAccountKey -AccountName $StorageAccountName -ResourceGroupName $saResourceGroup
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageAccountKey[0].value
 
+$blobName = "CompassExams.zip"
+$psTempDirectory = "D:\PSDownloads"
+$targetDirectory = "C:\Certiport\Compass"
+$containerName = "testingcpexams"
+
+Write-Information -MessageData 'Downloading files from sorage account' -InformationAction Continue
+Get-AzStorageBlobContent -Blob $blobName -Container $containerName -Destination $psTempDirectory -Context $ctx -Force
+
+Write-Information -MessageData 'Extracting downloaded files' -InformationAction Continue
+Expand-Archive -Path $psTempDirectory'\compassexams.zip' -DestinationPath $targetDirectory -Force
+
+# Disconnect Azure account
+Write-Information -MessageData 'Disconnecting from Azure' -InformationAction Continue
+Disconnect-AzAccount
+
+### Download and install SecureClient for City and Guilds
+Invoke-WebRequest -Uri https://evolve.cityandguilds.com/secureassess/SecureClientinstaller.msi -OutFile D:\PSDownloads\SecureClientinstaller.msi
+
+Write-Information -MessageData 'Install the City and Guild SecureClient' -InformationAction Continue
 $cgArgs = @('/quiet','/norestart')
-Start-Process -FilePath "C:\PSDownloads\SecureClientinstaller.msi" -ArgumentList $cgArgs
+Start-Process -FilePath "D:\PSDownloads\SecureClientinstaller.msi" -ArgumentList $cgArgs -Wait
+
+#### Clean up folder
+Remove-Item 'D:\PSDownloads' -Recurse
--- AIB/buildArtifacts/createLabEnvironmentServer.ps1
+++ AIB/buildArtifacts/createLabEnvironmentServer.ps1
@@ -1,24 +1,33 @@
 # Download and configure the required lab environment artifacts
 ## Create the ISO images folder for Hyper-V
+Write-Information -MessageData 'Creating Hyper-V ISO directory' -InformationAction Continue
 New-Item C:\ISOs -ItemType Directory
 
-### Connect to Azure storage
-$sasToken = New-AzStorageContainerSASToken -Container testingiso -Permission r1
-$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -SasToken $sasToken
+# Connect to Azure Account
+Write-Information -MessageData 'Connecting to the Azure storage account' -InformationAction Continue
+Connect-AzAccount
+
+#Selecting the subscription if you have access to more than one subscriptions in your Azure account
+#$context = Get-AzSubscription -SubscriptionId ""
+#Set-AzContext $context
+
+### Connect to Azure storage and Set the context as per the security requirements (i will be trying different options to see which is best)
+$StorageAccountName = "balticaibsa"
+$saResourceGroup = "BalticImagesRg"
+
+$storageAccountKey = Get-AzStorageAccountKey -AccountName $StorageAccountName -ResourceGroupName $saResourceGroup
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageAccountKey[0].value
 
 ### Download ISO images
-$blobName = @('Windows_Server_2016_Datacenter.iso','Windows_10_Pro.iso','Office2013.iso')
-#$blobName1 = "Windows_Server_2016_Datacenter.iso"
-#$blobName2 = "Windows_10_Pro.iso"
-#$blobName3 = "Office2013.iso"
-
-$targetDirectory = "C:\ISOs"
 $containerName = "testingiso"
+$blobs = Get-AzStorageBlob -Container $containerName -Context $ctx
+$targetDirectory = "C:\ISOs"
 
-Get-AzStorageBlobContent -Blob $blobName -Container $containerName -Destination $targetDirectory -Context $ctx
-#Get-AzStorageBlobContent -Blob $blobName1 -Container $containerName -Destination $targetDirectory -Context $ctx -wait
-#Get-AzStorageBlobContent -Blob $blobName2 -Container $containerName -Destination $targetDirectory -Context $ctx -wait
-#Get-AzStorageBlobContent -Blob $blobName3 -Container $containerName -Destination $targetDirectory -Context $ctx
-#Invoke-WebRequest -Uri -OutFile C:\ISOs\Windows_Server_2016_Datacenter
-#Invoke-WebRequest -Uri -OutFile C:\ISOs\Windows_10_Pro
-#Invoke-WebRequest -Uri -OutFile C:\ISOs\Office2013
+Write-Information -MessageData 'Downloading the ISO files' -InformationAction Continue
+foreach ($blob in $blobs) {
+    Get-AzStorageBlobContent -Container $containerName -Blob $blob.Name -Destination $targetDirectory -Context $ctx
+}
+
+# Disconnect from Azure
+Write-Information -MessageData 'Disconnecting from Azure' -InformationAction Continue
+Disconnect-AzAccount
