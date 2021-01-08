96f3eb4752f1f099cdb1b8e8fc26da2fc3f9982b
Chris Langford <clangford@lang-ford.uk>
2021-01-08 19:39:49 +0000

    Some changes

--- Development/SecureAssess/installSecureClientGUI.ps1
+++ Development/SecureAssess/installSecureClientGUI.ps1
@@ -22,7 +22,7 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     $prompt = [Security.Windows.MessageBox]::Show($ask, "Run as an Administrator or not?", $button, $errorIcon)
     switch ($prompt) {
         Yes {
-            Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+            Write-Output "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
             Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
             Exit
         }
@@ -34,32 +34,64 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 
 # The GUI using Windows Forms
 $form = New-Object System.Windows.Forms.Form
-$form.ClientSize = New-Object System.Drawing.Point(1050,700)
-$form.Text = "Form"
+$form.ClientSize = New-Object System.Drawing.Point(500,500)
+$form.Text = "City & Guilds SecureClient"
 $form.TopMost = $false
+$form.ShowIcon = $false
 
 $panel1 = New-Object System.Windows.Forms.Panel
-$panel1.Height = 156
-$panel1.Width = 1032
-$panel1.Location = New-Object System.Drawing.Point(9,90)
+$panel1.Height = 72
+$panel1.Width = 428
+$panel1.Location = New-Object System.Drawing.Point(31,31)
+$panel1.TabIndex = 0
 
 $label1 = New-Object System.Windows.Forms.Label
-$label1.Text = "Update City and Guilds Secure Assess Client"
+$label1.Text = "Update or Install City and Guilds Secure Client"
 $label1.AutoSize = $true
-$label1.Width = 25
-$label1.Height = 10
-$label1.Location = New-Object System.Drawing.Point(10,30)
-$label1.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',30)
+#$label1.Width = 422
+#$label1.Height = 57
+$label1.Size = New-Object System.Drawing.Size(422,57)
+$label1.Location = New-Object System.Drawing.Point(3,7)
+$label1.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',18)
+$label1.TabIndex = 0
+$label1.TextAlign = [System.Drawing.ContentAlignment]::TopCenter
+$label1.UseCompatibleTextRendering = $true
 
 $updateSecureClient = New-Object System.Windows.Forms.Button
 $updateSecureClient.Text = "Update SecureClient"
-$updateSecureClient.Width = 200
-$updateSecureClient.Height = 115
-$updateSecureClient.Location = New-Object System.Drawing.Point(16,19)
-$updateSecureClient.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',30)
+$updateSecureClient.TabIndex = 1
+$updateSecureClient.Width = 175
+$updateSecureClient.Height = 145
+$updateSecureClient.Location = New-Object System.Drawing.Point(31,128)
+$updateSecureClient.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',16)
 
-$form.controls.AddRange(@($panel1,$label1))
-$panel1.controls.AddRange(@($updateSecureClient))
+$installSecureClinet = New-Object System.windows.Forms.Button
+$installSecureClinet.Text = "Install SecureClient"
+$installSecureClinet.TabIndex = 0
+$installSecureClinet.Width = 175
+$installSecureClinet.Height = 145
+$installSecureClinet.Location = New-Object System.Drawing.Point(284,128)
+$installSecureClinet.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',16)
+
+#Logo Panel
+$panel2 = New-Object System.Windows.Forms.Panel
+$panel2.Width = 425
+$panel2.Height = 125
+$panel2.Location = New-Object System.Drawing.Point(31,310)
+$panel2.TabIndex = 3
+
+$logoBox = New-Object System.Windows.Forms.PictureBox
+$logoBox.Width = 200
+$logoBox.Height = 118
+$logoBox.Location = New-Object System.Drawing.Point(117,3)
+$logoBox.imageLocation = "https://github.com/ChrisTitusTech/win10script/blob/master/titus-toolbox.png?raw=true"
+$logoBox.SizeMode = [System.Windows.Forms.PictureBoxSizeMode]::zoom
+$logoBox.TabIndex = 3
+
+
+
+$form.controls.AddRange(@($panel1,$label1,$panel2,$logoBox))
+$panel1.controls.AddRange(@($updateSecureClient,$installSecureClinet))
 
 $updateSecureClient.Add_Click({
     $secureClient = (Test-Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*').DisplayName -Match "SecureClient"
@@ -68,8 +100,22 @@ $updateSecureClient.Add_Click({
         Start-Process msiexec.exe -Wait -ArgumentList '/i "C:\PSDownloads\SecureClientinstaller.msi" /qn'
         $wShell.popup("Update completed",0,"Done",0x0)
     }else{
-        $wShell.popup("SecureClient is not installed. Please install the client",0,"Done",0x0)
+        $wshell.popup("SecureClient is not installed. Please install the client",0,"Done",0x0)
     }
+})
+
+$installSecureClinet.Add_Click({
+    $dotNet35 = "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5"
+    if (!(Test-Path $dotNet35)){
+        Write-Output "Installing .Net Framework 3.5"
+        Enable-WindowsOptionalFeature -FeatureName "NetFx3" -Online        
+    }
+    else{        
+        Write-Output ".Net Framework 3.5 is already installed"
+    }
+    Write-Output "Installing SecureClinet_CANDG"
+    Start-Process msiexec.exe -Wait -ArgumentList '/i "C:\PSDownloads\SecureClientinstaller.msi" /qn'
+    $wshell.Popup("Installation Comppleted",0,"Done",0x0)
 })
 
 [void]$form.ShowDialog()
